4e00b3b014a82aa7f568f69b7f747f94d67964b8
刘林林 <7796788+liu_linlin@user.noreply.gitee.com>
2020-07-31 16:36:47 +0800

    javascript

---
+++ typeof.js
@@ -0,0 +1,7 @@
+console.log(typeof([]));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(window));
+console.log(typeof(Number));
+console.log(typeof(String));
+console.log(typeof(Array));
